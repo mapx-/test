408737455681d7758753c5a7398cd2dfb9a78498
mapx <10303732+mapx-@users.noreply.github.com>
2023-02-11 22:47:05 +0100

    new scriptlet  - replace
    replace for inline scripts

---
+++ repl.js
@@ -0,0 +1,50 @@
+
+// replace for inline scripts
+// example filter:
+// hipsonyc.com##+js(rpx, /click.*_blank.*location\.href/, /^window\.location\.href.*\'$/, )
+//
+// example.com##+js(rpx, param1, param2, param3)
+// param1 (regex): matching a specific inline script
+// param2 regular expression, to search for
+// param3 The new value (to replace with)
+
+/// rpx.js
+    (function() {
+        'use strict';
+          let needle = '{{1}}';
+          if ( needle === '' || needle === '{{1}}' ) {
+              needle = '';
+          } else if ( needle.startsWith('/') && needle.endsWith('/') ) {
+              needle = needle.slice(1,-1);
+          } else {
+              needle = needle.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+          }
+          needle = new RegExp(needle, "gms");
+          let what = '{{2}}';
+          if ( what === '' || what === '{{2}}' ) {
+              what = '';
+          } else if ( what.startsWith('/') && what.endsWith('/') ) {
+              what = what.slice(1,-1);
+          } else {
+              what = what.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+          }
+          what = new RegExp(what, "gms");
+          let by = '{{3}}';
+          if ( by === '' || by === '{{3}}' ) {
+              by = '';
+          }
+          const obs = new MutationObserver((mutationsList) => {
+			  for (const mut of mutationsList) {
+				  for (const node of mut.addedNodes) {
+					  if (node.tagName === 'SCRIPT') {
+						  if (needle.test(node.textContent) ) {
+							  let ttt = node.textContent;
+							  ttt  = ttt.replace(what, by);
+							  node.textContent = ttt;
+						  }
+					  }
+				  }
+			  }
+          });
+          obs.observe(document.documentElement, { childList: true, subtree: true });
+    })();
