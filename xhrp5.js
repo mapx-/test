f42174aedadd5f448025280c62f0913c9cb98417
mapx <10303732+mapx-@users.noreply.github.com>
2023-09-06 18:36:47 +0200

    Update xhrp5.js

--- xhrp5.js
+++ xhrp5.js
@@ -7,15 +7,35 @@
 // param2 regular expression, to search for
 // param3 The new value (to replace with)
 /// xhrp.js
-/// dependency safe-self.fn
-function xhrp(urlXHR = '', what = '', by = '') {
-          const reUrlXHR = safe.patternToRegex(urlXHR);
-          const reWhat = safe.patternToRegex(what, 'gms');
-		  //console.log('uBO:'+'reUrlXHR:'+reUrlXHR);
-          //console.log('uBO:'+'reWhat:'+reWhat);
+    (function() {
+        'use strict';
+          let urlXHR = '{{1}}';
+          if ( urlXHR === '' || urlXHR === '{{1}}' ) {
+              urlXHR = '';
+          } else if ( urlXHR.startsWith('/') && urlXHR.endsWith('/') ) {
+              urlXHR = urlXHR.slice(1,-1);
+          } else {
+              urlXHR = urlXHR.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+          }
+          urlXHR = new RegExp(urlXHR);
+          let what = '{{2}}';
+          if ( what === '' || what === '{{2}}' ) {
+              what = '';
+          } else if ( what.startsWith('/') && what.endsWith('/') ) {
+              what = what.slice(1,-1);
+          } else {
+              what = what.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+          }
+          what = new RegExp(what, "gms");
+          let by = '{{3}}';
+          if ( by === '' || by === '{{3}}' ) {
+              by = '';
+          }
+          //console.log('uBO:'+'urlXHR:'+urlXHR);
+          //console.log('uBO:'+'what:'+what);
           //console.log('uBO:'+'by:'+by);
     const pruner = text => {
-    	text  = text.replace(reWhat, by);
+    	text  = text.replace(what, by);
         return text;
     };
     const urlFromArg = arg => {
@@ -26,7 +46,7 @@ function xhrp(urlXHR = '', what = '', by = '') {
     const realFetch = self.fetch;
     self.fetch = new Proxy(self.fetch, {
         apply: function(target, thisArg, args) {
-            if ( reUrlXHR.test(urlFromArg(args[0])) === false ) {
+            if ( urlXHR.test(urlFromArg(args[0])) === false ) {
                 return Reflect.apply(target, thisArg, args);
             }
             return realFetch(...args).then(realResponse =>
@@ -42,7 +62,7 @@ function xhrp(urlXHR = '', what = '', by = '') {
     });
     self.XMLHttpRequest.prototype.open = new Proxy(self.XMLHttpRequest.prototype.open, {
         apply: async (target, thisArg, args) => {
-            if ( reUrlXHR.test(urlFromArg(args[1])) === false ) {
+            if ( urlXHR.test(urlFromArg(args[1])) === false ) {
                 return Reflect.apply(target, thisArg, args);
             }
             thisArg.addEventListener('readystatechange', function() {
@@ -61,6 +81,6 @@ function xhrp(urlXHR = '', what = '', by = '') {
                 Object.defineProperty(thisArg, 'responseText', { value: textout });
             });
             return Reflect.apply(target, thisArg, args);
-        }
+   }
     });
-}
+})();
